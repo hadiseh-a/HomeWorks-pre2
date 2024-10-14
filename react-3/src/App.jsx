292edaebfe02164843500b0061dea874f9ec7a8d
hadiseh-a <hadisea82@gmail.com>
2024-10-14 14:07:11 +0330

    done

--- react-3/src/App.jsx
+++ react-3/src/App.jsx
@@ -5,6 +5,7 @@ import "./App.css";
 import Counter from "../1-counter/Counter";
 import ToggleTheme from "../2-toggle-theme/ToggleTheme";
 import "../2-toggle-theme/app.css";
+import LikeAndDeslikeButtons from "../3-likeAndDeslike/LikeAndDeslikeButtons";
 function App() {
 
   return (
@@ -12,7 +13,9 @@ function App() {
       {/* exercise1 */}
       {/* <Counter/> */}
       {/* exercise2 */}
-      <ToggleTheme />
+      {/* <ToggleTheme /> */}
+      {/* exercise3 */}
+      <LikeAndDeslikeButtons/>
     </>
   );
 }
