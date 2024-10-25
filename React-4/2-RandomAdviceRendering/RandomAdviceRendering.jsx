a3f1bbbc888e148c2717c51ce5f90333718d97d0
hadiseh-a <hadisea82@gmail.com>
2024-10-25 17:14:19 +0330

    done

--- React-4/2-RandomAdviceRendering/RandomAdviceRendering.jsx
+++ React-4/2-RandomAdviceRendering/RandomAdviceRendering.jsx
@@ -3,12 +3,23 @@ import { useState, useEffect } from "react";
 function RandomAdviceRendering() {
   const [advice, setAdvice] = useState("");
   useEffect(() => {
-    async () => {
+    const getAdvice = async () => {
       const response = await fetch("https://api.adviceslip.com/advice");
       const data = await response.json();
       setAdvice(data.slip.advice);
     };
+    getAdvice();
   }, []);
-  return <h6 style={{backgroundColor: "palegreen", borderRadius:"2px", color:"green"}}>this is your Advice: {advice}</h6>;
-} 
+  return (
+    <h6
+      style={{
+        backgroundColor: "palegreen",
+        borderRadius: "2px",
+        color: "green",
+      }}
+    >
+      this is your Advice: {advice}
+    </h6>
+  );
+}
 export default RandomAdviceRendering;
